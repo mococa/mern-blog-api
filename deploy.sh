547170fab29cedd5bc3a20c02d46ed952896d815
mococa <xfazxtudo@gmail.com>
2021-12-26 01:57:25 +0000

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
+getEnv(){
+    echo $(grep $1 .env | cut -d '=' -f2)
+}
+
 ## Config
-SID="$1"
-REPL_ID="$2"
+SID=$(getEnv SID)
+REPL_ID=$(getEnv REPL_ID)
 COMMAND="sh%20./pull.sh"
-REPLIT_API="$3"
+REPLIT_API=$(getEnv REPLIT_API)
 
 ## Execution
 URL="$REPLIT_API/$REPL_ID?sid=$SID&command=$COMMAND"
-
+echo $URL
 echo "$(curl $URL)"
 exit 1
